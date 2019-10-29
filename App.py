68f7b0d957e16e206abe68e5815d628485884977
Diego Campana <xxd13g018xx@gmail.com>
2019-10-29 07:34:04 -0500

    python archivo

---
+++ App.py
@@ -0,0 +1,17 @@
+n_personas = int(input("ingrese el numero de personas en el domicilio: "))
+lista_personas = []
+lista_sub = []
+i = 0
+
+
+while (i<n_personas):
+    nombre = input("ingrese el nombre ede la persona: ")
+    edad = input("ingrese edad de la persona")
+    genero = input("ingrese si es hombre o mujer:")
+
+    i += 1
+
+
+
+    
+    
