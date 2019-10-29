9187f0f2a13d77ddb5711667bfcfc726e43d5e27
Diego Campana <xxd13g018xx@gmail.com>
2019-10-29 08:14:54 -0500

    porfijn

--- App.py
+++ App.py
@@ -3,6 +3,7 @@ os.system("cls")
 
 ## INPUT
 n_personas = int(input("Ingrese el número de personas en el domicilio: "))
+n_enfermos = 0
 lista_nombres = []
 lista_edades = []
 lista_generos = []
@@ -15,6 +16,15 @@ while (i<n_personas):
     nombre = input("Ingrese el nombre de la persona: ")
     edad = input("Ingrese edad de la persona: ")
     genero = input("Ingrese si es hombre o mujer :")
+    enfermo = input("esta enfermo? (si/no)")
+
+    if (enfermo == "si"):
+        n_enfermos += 1 
+
+    else :
+        print("que bueno que estes sano(a) "+ nombre) 
+    
+    
     lista_nombres.append(nombre.title())
     lista_edades.append(edad)
     lista_generos.append(genero)
