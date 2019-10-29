1a615d426df7f1a7b24b3749f6e0b2bc01b6a3da
alumno informatica <alumno@ciis.usil.edu.pe>
2019-10-29 07:38:56 -0500

    asd

--- App.py
+++ App.py
@@ -1,9 +1,12 @@
+## INPUT
 n_personas = int(input("ingrese el numero de personas en el domicilio: "))
 lista_personas = []
 lista_sub = []
 i = 0
 
 
+
+##PROCESO
 while (i<n_personas):
     nombre = input("ingrese el nombre ede la persona: ")
     edad = input("ingrese edad de la persona")
