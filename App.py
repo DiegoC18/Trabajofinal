5c4c0898e288fb57b24297d15a6b149675726b00
alumno informatica <alumno@ciis.usil.edu.pe>
2019-10-29 07:50:17 -0500

    asdasd

--- App.py
+++ App.py
@@ -8,7 +8,7 @@ i = 0
 
 
 ##PROCESO
-while (i<=n_personas.length()):
+while (i<=len(n_personas)):
     nombre = input("ingrese el nombre ede la persona: ")
     edad = input("ingrese edad de la persona: ")
     genero = input("ingrese si es hombre o mujer :")
