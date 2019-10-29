3b2441a260ef3a01938d7b05a83839ea8ff74ccb
alumno informatica <alumno@ciis.usil.edu.pe>
2019-10-29 08:45:19 -0500

    vfin

--- App.py
+++ App.py
@@ -19,63 +19,33 @@ while (i<n_personas):
 
     nombre = input("ingrese el nombre de la persona: ")
     edad = int(input("ingrese edad de la persona: "))
-    while(True):
-        if edad>0 and edad<18:
-            edad2="menor de edad"
-            break
-        elif edad>=18 and edad<65:
-            edad2="mayor de edad"
-            break
-        elif edad>=65:
-            edad2="tercera edad"
-            break
-        else:
-            print("ERROR")
-
-    genero = input("ingrese si es hombre o mujer :")
-
-    nombre = input("Ingrese el nombre de la persona: ")
-    edad = input("Ingrese edad de la persona: ")
-    def genero():
-        hask = input("Es la persona hombre?(sí o no):")
+    genero = input("Es la persona hombre?(sí o no):")
     if genero.title() == "Si" or "Sí":
-        return True
         genero = "Hombre"
     if genero.title() == "No":
-        return False
         genero = "Mujer"
    
-    genero = input("Ingrese si es hombre o mujer :")
     enfermo = input("esta enfermo? (si/no)")
 
     if (enfermo == "si"):
-        n_enfermos += 1 
+        n_enfermos += 1
+        enf_bool = "Si" 
 
     else :
         print("que bueno que estes sano(a) "+ nombre) 
+        enf_bool ="No"
     
-    
-    lista_nombres.append(nombre.title())
-    lista_edades.append(edad)
-    lista_edades2.append(edad2)
-    lista_generos.append(genero)
-
-
-    
-
+  
     i += 1
 
  ##SALIDA
-print(lista_nombres)
-print(lista_edades)
-print(lista_generos)
 
 for i in range (n_personas):
     print("---------------------")
     print("Persona #",i+1)
     print( "Nombre: ", lista_nombres[i] )
     print( "Edad: ", lista_edades[i] )
-    print(lista_edades2[i] )
+    print( "Enfermo: ", enf_bool )
     print( "Género: ", lista_generos[i] )
     print("---------------------")
     
