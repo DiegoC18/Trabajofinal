b1f8a5e38129303260e6dcd2a24bf220cd6c8b63
Manuel Rios <manu3554@gmail.com>
2019-10-30 08:36:59 -0500

    genero y enfermo funcionan

--- App.py
+++ App.py
@@ -6,47 +6,70 @@ print("---------- Bienvenido al sistema de registro de familias ----------")
 ## INPUT
 n_personas = int(input("Ingrese el número de personas en el domicilio: "))
 n_enfermos = 0
+genero = ""
 lista_nombres = []
 lista_edades = []
 lista_generos = []
-lista_edades2=[]
+lista_enfermos=[]
 i = 0
 
-
-
-##INPUT
 while (i<n_personas):
 
-    nombre = input("ingrese el nombre de la persona: ")
-    edad = int(input("ingrese edad de la persona: "))
-    genero = input("Es la persona hombre?(sí o no):")
-    if genero.title() == "Si" or "Sí":
-        genero = "Hombre"
-    if genero.title() == "No":
-        genero = "Mujer"
-   
-    enfermo = input("¿esta enfermo? (si/no)")
+    nombre = input("Ingrese el nombre de la persona: ")
 
-    if (enfermo == "si"):
-        n_enfermos += 1
-        enf_bool = "Si" 
+    edad = int(input("Ingrese edad de la persona: "))  
 
-    else :
-        print("que bueno que estes sano(a) "+ nombre) 
-        enf_bool ="No"
-    
+    while True:
+
+        gen_pregunta = input("Es la persona hombre? (sí o no): ")
+
+        if gen_pregunta.title() == "si":
+            genero = "Hombre"
+            lista_generos.append(genero)
+            break
+
+        if gen_pregunta.lower() == "no":
+            genero = "Mujer"
+            lista_generos.append(genero)
+            break
+
+        else :
+            print("Ingrese una respuesta válida (Sí/No).")
+
+    while True:
+
+        enfermo = input("¿Está la persona enferma? (Sí/No): ")
+
+        if (enfermo.lower() == "si"):
+            n_enfermos += 1
+            lista_enfermos.append("Sí")
+            break
+
+        if (enfermo.lower() == "no"):
+            print("Qué bueno que estés sano(a) "+ nombre) 
+            lista_enfermos.append("No")
+            break
+
+        else :
+            print("Ingrese una respuesta válida (si/no).")
+                
   
     i += 1
 
- ##SALIDA
+    lista_nombres.append(nombre)
+    lista_edades.append(edad)
+
+##SALIDA
 
 for i in range (n_personas):
     print("---------------------")
     print("Persona #",i+1)
     print( "Nombre: ", lista_nombres[i] )
     print( "Edad: ", lista_edades[i] )
-    print( "Enfermo: ", enf_bool )
+    print( "Enfermo: ", lista_enfermos[i] )
     print( "Género: ", lista_generos[i] )
     print("---------------------")
+    print(lista_generos)
+print("Número total de enfermos: ", n_enfermos)
     
     
